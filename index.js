5dd59539987b45f8499a8f0be504795b5bf00221
ChrisEs146 <chris.es146@gmail.com>
2022-08-01 22:36:56 -0600

    Add: server inputs

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
