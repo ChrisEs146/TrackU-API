58eeab8982e506a63545d20e0084e58837085c94
ChrisEs146 <chris.es146@gmail.com>
2022-08-02 21:42:26 -0600

    Add: DB connection

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ app.use("/user", userRoutes);
 
 const DB_CONNECTION = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.error(error));
