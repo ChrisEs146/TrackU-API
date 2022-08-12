ae93eba2bd043b159090488bb1bb9c5fe426a60d
ChrisEs146 <chris.es146@gmail.com>
2022-08-11 19:24:04 -0600

    Delete: body parser

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import userRoutes from "./routes/users.js";
 dotenv.config({ path: "./config.env" });
+
+import userRoutes from "./routes/users.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/user", userRoutes);
 
