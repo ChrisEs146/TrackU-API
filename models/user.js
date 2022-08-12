32e4911f89613ca964c91b40f928ef7a394b8301
ChrisEs146 <chris.es146@gmail.com>
2022-08-11 19:20:04 -0600

    Delete: id field

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const userSchema = mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  id: { type: String, required: true },
 });
 
 export default mongoose.model("User", userSchema);
