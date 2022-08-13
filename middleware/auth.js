242064116b898cb8b498a74981def866da2d9e87
ChrisEs146 <chris.es146@gmail.com>
2022-08-12 22:29:43 -0600

    Add: Auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,27 @@
 import jwt from "jsonwebtoken";
 import user from "../models/user";
 
-const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
+export const auth = async (req, res, next) => {
+  let userToken;
+  const authHeader = req.headers.authorization;
 
-    let decodedData;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    try {
+      token = authHeader.split(" ")[1];
 
-    if (token && isCustomAuth) {
-      decodedData = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = decodedData?.id;
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await user.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Invalid Token.");
     }
+  }
 
-    next();
-  } catch (error) {
-    console.error(error);
+  if (!token) {
+    res.status(401);
+    throw new Error("Token not found");
   }
 };
-
-export default auth;
