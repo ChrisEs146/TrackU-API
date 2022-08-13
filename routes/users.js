a523ff9dae095cc4a21a13eecb95cb5e4358ad89
ChrisEs146 <chris.es146@gmail.com>
2022-08-12 23:51:44 -0600

    Update:  endpoint to get user info

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/signin", signIn);
 router.post("/signup", signUp);
-router.get("/userData", auth, getUser);
+router.get("/info", auth, getUser);
 
 export default router;
