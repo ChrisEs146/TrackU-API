e6c9bdf8a31aab2a2d37243926397f3bef453e29
ChrisEs146 <chris.es146@gmail.com>
2022-08-12 22:32:34 -0600

    add: Throwing error if user doesn't exists

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,10 @@ export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (!existingUser) return res.status(404).json({ message: "User doesn't exist." });
+    if (!existingUser) {
+      res.status(404);
+      throw new Error("User does not exist.");
+    }
 
     const isValidPassword = await bcrypt.compare(password, existingUser.password);
     if (!isValidPassword) return res.status(400).json({ message: "Invalid Credentials." });
