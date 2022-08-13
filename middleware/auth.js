1e7a1bae647c187d2a4a0d75d4d61d63bc1e026f
ChrisEs146 <chris.es146@gmail.com>
2022-08-12 22:39:41 -0600

    Fix: Typo in userToken variable

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ export const auth = async (req, res, next) => {
 
   if (authHeader && authHeader.startsWith("Bearer")) {
     try {
-      token = authHeader.split(" ")[1];
+      userToken = authHeader.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
