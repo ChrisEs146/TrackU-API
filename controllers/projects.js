3aa43e10adf4c51208bdee5456475f1329b764c0
ChrisEs146 <chris.es146@gmail.com>
2022-09-21 14:55:02 -0600

    Add: Controller to add a project

--- controllers/projects.js
+++ controllers/projects.js
@@ -16,3 +16,27 @@ export const getAllProjects = async (req, res, next) => {
     next(error);
   }
 };
+
+/**
+ * Controller to add a project.
+ * @route POST /projects
+ * @access Private
+ */
+export const addProject = async (req, res, next) => {
+  const { title, description } = req.body;
+  const { id } = req.user;
+
+  try {
+    // Checking for empty fields
+    if (!title || !description) {
+      res.status(400);
+      throw new Error("Fields cannot be empty");
+    }
+
+    //  Creating project and sending response
+    const project = await Project.create({ user: id, title: title, description: description });
+    res.status(200).json(project);
+  } catch (error) {
+    next(error);
+  }
+};
