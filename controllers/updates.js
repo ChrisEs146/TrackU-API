669148e5ff3fcfaa45c106c848eaf9327b1d7584
ChrisEs146 <chris.es146@gmail.com>
2022-09-22 16:13:59 -0600

    Add: Controller to add an update to a project

--- controllers/updates.js
+++ controllers/updates.js
@@ -17,3 +17,31 @@ export const getUpdates = async (req, res, next) => {
   }
 };
 
+/**
+ * Controller to add an update to a project.
+ * @route POST /upddates
+ * @access Private
+ */
+export const addUpdate = async (req, res, next) => {
+  const { title, description } = req.body;
+  const { projectId } = req.body;
+
+  try {
+    // Checking for empty fields
+    if (!title || !description) {
+      res.status(400);
+      throw new Error("Fields cannot be empty");
+    }
+
+    // Creating new update
+    const update = await Update.create({
+      project: projectId,
+      title: title,
+      description: description,
+    });
+    res.status(200).json(update);
+  } catch (error) {
+    next(error);
+  }
+};
+
