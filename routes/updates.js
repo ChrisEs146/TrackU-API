57b3472fbdca8a07225a698ada346b6567359987
ChrisEs146 <chris.es146@gmail.com>
2022-09-27 11:21:50 -0600

    Update: updates routes

--- routes/updates.js
+++ routes/updates.js
@@ -1,17 +1,19 @@
 import express from "express";
-import { auth } from "../middleware/auth";
+import { auth } from "../middleware/auth.js";
 import {
   getUpdates,
   addUpdate,
   getUpdate,
-  modifyUpdate,
+  editUpdate,
   deleteUpdate,
 } from "../controllers/updates.js";
 
 const router = express.Router();
 
-router.get("/", auth, getUpdates);
-router.post("/", auth, addUpdate);
-router.get("/:updateId", auth, getUpdate);
-router.put("/:updateId", auth, modifyUpdate);
-router.delete("/:updateId", auth, deleteUpdate);
+router.get("/:projectId", auth, getUpdates);
+router.post("/:projectId", auth, addUpdate);
+router.get("/project/:projectId/update/:updateId", auth, getUpdate);
+router.put("/project/:projectId/update/:updateId", auth, editUpdate);
+router.delete("/project/:projectId/update/:updateId", auth, deleteUpdate);
+
+export default router;
