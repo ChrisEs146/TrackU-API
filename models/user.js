44da77c1cc1f752b7269070632e18ac48ef382f9
ChrisEs146 <chris.es146@gmail.com>
2022-10-09 15:54:52 -0600

    Add: Trigger to delete all projects and updates
    - Deletes all projects and updates when a user is deleted.

--- models/user.js
+++ models/user.js
@@ -7,5 +7,12 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+// Delete on cascade-like trigger
+userSchema.pre("remove", async function () {
+  const projects = await Project.find({ user: this._id }).exec();
+  const projectsId = projects.map((project) => project._id);
+  await Update.deleteMany({ project: { $in: projectsId } }).exec();
+  await Project.deleteMany({ user: this._id }).exec();
+});
 
 export default mongoose.model("User", userSchema);
