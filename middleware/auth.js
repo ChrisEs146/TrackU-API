c9cf88bb6f6887e45436064ab900f325bbdda5d4
ChrisEs146 <chris.es146@gmail.com>
2022-10-15 10:32:12 -0600

    Update: auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,23 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 export const auth = (req, res, next) => {
   const authHeaders = req.headers.authorization || req.headers.Authorization;
   try {
     if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
-      res.status(401);
-      throw new Error("Unauthorized");
+      res.status(401).json({ message: "Unauthorized" });
     }
 
     const token = authHeaders.split(" ")[1];
     if (!token) {
-      res.status(403);
-      throw new Error("Token not found");
+      res.status(403).json({ message: "Token not found" });
     }
 
-    jwt.verify(token, process.env.ACCESS_SECRET, (error, decoded) => {
+    jwt.verify(token, process.env.ACCESS_SECRET, async (error, decoded) => {
       if (error) {
-        res.status(401);
-        throw new Error("Invalid Token");
+        return res.status(401).json({ message: "Invalid Token" });
       }
-      req.user = decoded;
+      req.user = await User.findById(decoded._id).exec();
       next();
     });
   } catch (error) {
