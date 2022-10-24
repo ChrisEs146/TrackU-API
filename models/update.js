11701f9118fb2d77157683cb892495369bd85b7c
ChrisEs146 <chris.es146@gmail.com>
2022-10-24 17:34:29 -0600

    Add: validation to update model

--- models/update.js
+++ models/update.js
@@ -3,8 +3,19 @@ import mongoose from "mongoose";
 const updateSchema = mongoose.Schema(
   {
     project: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Project" },
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+      minlength: [4, "Title should have at least 4 characters."],
+      maxlength: [50, "Title cannot have more than 50 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      minlength: [4, "Description cannot have less than 4 characters"],
+      maxlength: [800, "Description cannot have more than 800 characters"],
+    },
   },
   {
     timestamps: true,
