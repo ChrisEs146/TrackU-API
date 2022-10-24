51179ce8a6d8a6ad662330fd51f06e457b90324e
ChrisEs146 <chris.es146@gmail.com>
2022-10-24 17:33:26 -0600

    Add: Validation to user model

--- models/user.js
+++ models/user.js
@@ -3,10 +3,38 @@ import Project from "./project.js";
 import Update from "./update.js";
 
 const userSchema = mongoose.Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  fullName: {
+    type: String,
+    required: [true, "Fullname is required"],
+    trim: true,
+    minlength: [4, " Fullname should have at least 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    validate: {
+      validator: function (value) {
+        return /^(?=\S)(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#%^&*])[a-zA-Z0-9!@#%^&*]{8,20}$/.test(
+          value
+        );
+      },
+      message: "Invalid Password",
+    },
+  },
 });
+
 // Delete on cascade-like trigger
 userSchema.pre("remove", async function () {
   const projects = await Project.find({ user: this._id }).exec();
