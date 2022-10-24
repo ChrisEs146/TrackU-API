1704a3242c224e468ee1b2b58cee3ede2f861f79
ChrisEs146 <chris.es146@gmail.com>
2022-10-24 17:35:38 -0600

    Update: User controller to handle validation errs

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.js";
 import Jwt from "jsonwebtoken";
 import { createToken } from "../utils/tokenCreator.js";
+import { getError } from "../utils/getError.js";
 
 /**
  * Controller to sign in a user.
@@ -10,14 +11,15 @@ import { createToken } from "../utils/tokenCreator.js";
  */
 export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
-  try {
-    // Checking for possible blank fields
-    if (!email || !password) {
-      return res.status(400).json({ message: "Fields cannot be empty." });
-    }
 
+  // Checking for possible blank fields
+  if (!email || !password) {
+    return res.status(400).json({ message: "Fields cannot be empty." });
+  }
+
+  try {
     // Finding user
-    const existingUser = await User.findOne({ email }).lean().exec();
+    const existingUser = await User.findOne({ email: email.toLowerCase() }).lean().exec();
     if (!existingUser) {
       return res.status(404).json({ message: "User does not exist" });
     }
@@ -37,7 +39,7 @@ export const signIn = async (req, res, next) => {
       true
     );
 
-    res
+    return res
       .status(200)
       .cookie("token", refreshToken, { httpOnly: true, sameSite: "None", secure: true })
       .json({ accessToken });
@@ -53,35 +55,33 @@ export const signIn = async (req, res, next) => {
  */
 export const signUp = async (req, res, next) => {
   const { fullName, email, password, confirmPassword } = req.body;
-  try {
-    // Checking for possible blank fields
-    if (!fullName || !email || !password || !confirmPassword) {
-      return res.status(400).json({ message: "Fields cannot be empty." });
-    }
 
-    // Checking for existing user
-    const existingUser = await User.findOne({ email }).lean().exec();
+  // Checking for possible blank fields
+  if (!fullName || !email || !password || !confirmPassword) {
+    return res.status(400).json({ message: "Fields cannot be empty." });
+  }
+
+  // Checking if passwords match
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match." });
+  }
+
+  // Checking for existing user
+  try {
+    const existingUser = await User.findOne({ email: email.toLowerCase() }).lean().exec();
     if (existingUser) {
       return res.status(400).json({ message: "User already exists." });
     }
-
-    // Checking if passwords match
-    if (password !== confirmPassword) {
-      return res.status(400).json({ message: "Passwords do not match." });
-    }
-
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const user = await User.create({ fullName, email, password: hashedPassword });
-
-    res.status(201).json({
-      _id: user._id,
-      fullName: user.fullName,
-      email: user.email,
-    });
   } catch (error) {
     next(error);
+  }
+
+  // Creating and validating user data
+  try {
+    const user = await User.create({ fullName, email, password });
+    return res.status(200).json({ _id: user._id, fullName: user.fullName, email: user.email });
+  } catch (error) {
+    return res.status(400).json({ message: getError(error) });
   }
 };
 
@@ -93,27 +93,34 @@ export const signUp = async (req, res, next) => {
 export const updateUsername = async (req, res, next) => {
   const { newFullName } = req.body;
   const userId = req.user._id;
-  try {
-    // Checking for possible blank field
-    if (!newFullName) {
-      return res.status(400).json({ message: "Fields cannot be empty." });
-    }
 
-    // Finding user
+  // Checking for possible blank field
+  if (!newFullName) {
+    return res.status(400).json({ message: "Fields cannot be empty." });
+  }
+
+  // Finding user
+  try {
     const existingUser = await User.findById(userId).exec();
     if (!existingUser) {
       return res.status(404).json({ message: "User not found" });
     }
+  } catch (error) {
+    next(error);
+  }
 
-    // Updating user's name
-    existingUser.fullName = newFullName;
-    const updatedUser = await existingUser.save();
-
-    res
+  // Updating user's name
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { fullName: newFullName },
+      { new: true, runValidators: true }
+    );
+    return res
       .status(200)
       .json({ _id: updatedUser._id, fullName: updatedUser.fullName, email: updatedUser.email });
   } catch (error) {
-    next(error);
+    return res.status(400).json({ message: getError(error) });
   }
 };
 
@@ -125,21 +132,22 @@ export const updateUsername = async (req, res, next) => {
 export const updateUserPassword = async (req, res, next) => {
   const { currentPassword, newPassword, confirmPassword } = req.body;
   const userId = req.user._id;
-  try {
-    // Checking for possible blank fields
-    if (!currentPassword || !newPassword || !confirmPassword) {
-      return res.status(400).json({ message: "Fields cannot be empty." });
-    }
 
+  // Checking for possible blank fields
+  if (!currentPassword || !newPassword || !confirmPassword) {
+    return res.status(400).json({ message: "Fields cannot be empty." });
+  }
+
+  // Checking if passwords match
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+
+  try {
     // Finding user
     const existingUser = await User.findById(userId).exec();
     if (!existingUser) {
       return res.status(404).json({ message: "User not found" });
-    }
-
-    // Checking if passwords match
-    if (newPassword !== confirmPassword) {
-      return res.status(400).json({ message: "Passwords do not match" });
     }
 
     // Checking if current password is valid
@@ -148,15 +156,14 @@ export const updateUserPassword = async (req, res, next) => {
       return res.status(400).json({ message: "Invalid Password" });
     }
 
-    // Hashing new password
-    const salt = await bcrypt.genSalt(10);
-    const newHashedPassword = await bcrypt.hash(newPassword, salt);
-
     // Updating user's password
-    existingUser.password = newHashedPassword;
-    await existingUser.save();
-
-    res.status(200).json({ message: "Password updated successfully" });
+    try {
+      existingUser.password = newPassword;
+      await existingUser.save();
+      return res.status(200).json({ message: "Password updated successfully" });
+    } catch (error) {
+      return res.status(400).json({ message: getError(error) });
+    }
   } catch (error) {
     next(error);
   }
@@ -171,14 +178,14 @@ export const deleteUser = async (req, res, next) => {
   const { email, password } = req.body;
   const userId = req.user._id;
 
-  try {
-    // Checking for possible empty fields
-    if (!email || !password) {
-      return res.status(400).json({ message: "Fields cannot be empty." });
-    }
+  // Checking for possible empty fields
+  if (!email || !password) {
+    return res.status(400).json({ message: "Fields cannot be empty." });
+  }
 
+  try {
     // Finding user
-    const existingUser = await User.findOne({ email }).exec();
+    const existingUser = await User.findOne({ email: email.toLowerCase() }).exec();
     if (!existingUser) {
       return res.status(404).json({ message: "User not found." });
     }
@@ -211,11 +218,11 @@ export const deleteUser = async (req, res, next) => {
 export const refresh = async (req, res, next) => {
   const cookieToken = req.cookies.token;
 
-  try {
-    if (!cookieToken) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
+  if (!cookieToken) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
+  try {
     const refreshToken = cookieToken;
     Jwt.verify(refreshToken, process.env.REFRESH_SECRET, async (error, decoded) => {
       if (error) {
@@ -249,7 +256,7 @@ export const logOut = (req, res) => {
  * @route GET /users/info
  * @access Private
  */
-export const getUser = async (req, res, next) => {
+export const getUser = (req, res, next) => {
   try {
     const user = req.user;
     res.status(200).json({ fullName: user.fullName, email: user.email });
