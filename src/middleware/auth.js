101613de393b977c19a1fdcbff960a971d9441b6
ChrisEs146 <chris.es146@gmail.com>
2022-10-27 17:38:28 -0600

    Update: auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,12 +5,12 @@ export const auth = (req, res, next) => {
   const authHeaders = req.headers.authorization || req.headers.Authorization;
   try {
     if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
-      res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     const token = authHeaders.split(" ")[1];
     if (!token) {
-      res.status(403).json({ message: "Token not found" });
+      return res.status(403).json({ message: "Token not found" });
     }
 
     jwt.verify(token, process.env.ACCESS_SECRET, async (error, decoded) => {
