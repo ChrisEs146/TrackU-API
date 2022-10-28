122e8905d4e1404475c7cb0f62c51350bd5f6050
ChrisEs146 <chris.es146@gmail.com>
2022-10-28 17:39:06 -0600

    Add: valid2 object to user mock data

--- tests/mockData/userMockData.js
+++ tests/mockData/userMockData.js
@@ -8,6 +8,12 @@ export const signUp = {
     password: "passWord14%",
     confirmPassword: "passWord14%",
   },
+  valid2: {
+    fullName: "Frank Doe",
+    email: "frank@email.com",
+    password: "passWord14%143",
+    confirmPassword: "passWord14%143",
+  },
   empty: {
     fullName: "",
     email: "",
@@ -56,12 +62,20 @@ export const signIn = {
 };
 
 /**
- * Testing utility function to create a user
+ * Testing utility function to create users.
+ * If no argument is given it will create user 1,
+ * otherwise if **extraUser** equals to true it will create user 2
  * @returns Object with new user and token
  */
-export const createUser = async function () {
-  const newUser = await User.create(signUp.valid);
-  if (newUser) {
+export const createUser = async function (extraUser = false) {
+  let newUser;
+  // User 1
+  if (!extraUser) {
+    newUser = await User.create(signUp.valid);
     return { newUser, token: createToken(newUser._id, newUser.fullName, newUser.email) };
   }
+
+  // User 2
+  newUser = await User.create(signUp.valid2);
+  return { newUser, token: createToken(newUser._id, newUser.fullName, newUser.email) };
 };
