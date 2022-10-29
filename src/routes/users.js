15634ca73079f28e0d8039ecdb346af551e6285a
ChrisEs146 <chris.es146@gmail.com>
2022-10-29 17:44:50 -0600

    Add: swagger documentation to user routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,7 +12,58 @@ import {
 import { auth } from "../middleware/auth.js";
 const router = express.Router();
 
+/**
+ * @swagger
+ * /users/signin:
+ *   post:
+ *     tags: [User]
+ *     summary: Sign in a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UserSignIn"
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/UserResponse"
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not found
+ */
 router.post("/signin", signIn);
+
+/**
+ * @swagger
+ * /users/signup:
+ *   post:
+ *     tags: [User]
+ *     summary: Register a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UserSignUp"
+ *     responses:
+ *       201:
+ *         description: Successfully Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/UserSignUpResponse"
+ *       400:
+ *         description: Bad request
+ *       409:
+ *         description: Conflict
+ */
 router.post("/signup", signUp);
 router.post("/logout", logOut);
 router.get("/refresh", refresh);
