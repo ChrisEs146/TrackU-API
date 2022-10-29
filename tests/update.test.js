f6ac2cf7f8f2dea758858ad7b68098012142b20f
ChrisEs146 <chris.es146@gmail.com>
2022-10-29 14:02:37 -0600

    Add: Update routes tests

--- tests/update.test.js
+++ tests/update.test.js
@@ -394,7 +394,460 @@ describe("Update", () => {
   });
 });
 // EDITING UPDATE
+
+describe("Update", () => {
+  describe("Successfully editing an update", () => {
+    it("Should return status 200", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.validEdit)
+        .expect(200);
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with empty data", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.empty)
+        .expect(400);
+      expect(body.message).toBe("Fields cannot be empty");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with short invalid title", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.invalidShortTitle)
+        .expect(400);
+      expect(body.message).toBe("Title should have at least 4 characters");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with long invalid title", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.invalidLongTitle)
+        .expect(400);
+      expect(body.message).toBe("Title cannot have more than 50 characters");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with short invalid description", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.invalidShortDescription)
+        .expect(400);
+      expect(body.message).toBe("Description cannot have less than 4 characters");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with long invalid description", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.invalidLongDescription)
+        .expect(400);
+      expect(body.message).toBe("Description cannot have more than 800 characters");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with invalid project ID", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id.concat(465)}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.validEdit)
+        .expect(400);
+      expect(body.message).toBe("Project ID is not valid");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update with invalid update ID", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id.concat(465)}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.validEdit)
+        .expect(400);
+      expect(body.message).toBe("Update ID is not valid");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update from nonexisting project", () => {
+    it("Should return status 404", async () => {
+      const { token } = await createUser();
+      const projectId2 = new mongoose.Types.ObjectId();
+
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${projectId2}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.validEdit)
+        .expect(404);
+      expect(body.message).toBe("Parent project not found");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update from nonexisting update", () => {
+    it("Should return status 404", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const updateId = new mongoose.Types.ObjectId();
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${updateId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.validEdit)
+        .expect(404);
+      expect(body.message).toBe("Update not found");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update from unauth project", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: project2 } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project2._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.validEdit)
+        .expect(400);
+      expect(body.message).toBe("Unauthorized Update");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Edit update without auth", () => {
+    it("Should return status 401", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .put(`/updates/project/${project._id}/update/${update._id}`)
+        .send(updateData.validEdit)
+        .expect(401);
+      expect(body.message).toBe("Unauthorized");
+    });
+  });
+});
+
 // DELETING UPDATE
+
+describe("Update", () => {
+  describe("Successfully deleting update", () => {
+    it("Should return status 200", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${project._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200);
+      expect(body.message).toBe("Update was deleted successfully");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Delete update with invalid project ID", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${project._id.concat(465)}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(400);
+      expect(body.message).toBe("Project ID is not valid");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Delete update with invalid update ID", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${project._id}/update/${update._id.concat(465)}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(400);
+      expect(body.message).toBe("Update ID is not valid");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Delete update from nonexisting project", () => {
+    it("Should return status 404", async () => {
+      const { token } = await createUser();
+      const projectId2 = new mongoose.Types.ObjectId();
+
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${projectId2}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(404);
+      expect(body.message).toBe("Parent project not found");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Delete update from nonexisting update", () => {
+    it("Should return status 404", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const updateId = new mongoose.Types.ObjectId();
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${project._id}/update/${updateId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(404);
+      expect(body.message).toBe("Update not found");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Delete update from unauth project", () => {
+    it("Should return status 400", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: project2 } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${project2._id}/update/${update._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(400);
+      expect(body.message).toBe("Unauthorized Update");
+    });
+  });
+});
+
+describe("Update", () => {
+  describe("Delete update without auth", () => {
+    it("Should return status 401", async () => {
+      const { token } = await createUser();
+      const { body: project } = await supertest(app)
+        .post("/projects")
+        .set("Authorization", `Bearer ${token}`)
+        .send(projectData.valid);
+
+      const { body: update } = await supertest(app)
+        .post(`/updates/${project._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(updateData.valid);
+
+      const { body } = await supertest(app)
+        .delete(`/updates/project/${project._id}/update/${update._id}`)
+        .expect(401);
+      expect(body.message).toBe("Unauthorized");
+    });
+  });
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
