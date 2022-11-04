b18eca419eeb78c083526f2364dfe1442731e1cc
ChrisEs146 <chris.es146@gmail.com>
2022-11-03 21:20:45 -0600

    Add: Schema swagger documentation

--- src/models/project.js
+++ src/models/project.js
@@ -1,5 +1,48 @@
 import mongoose from "mongoose";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Project:
+ *       type: object
+ *       required:
+ *         - user
+ *         - title
+ *         - description
+ *         - status
+ *         - progress
+ *       properties:
+ *         user:
+ *           type: string
+ *           description: Auto-generated user ID
+ *         title:
+ *           type: string
+ *           description: Project's title
+ *         description:
+ *           type: string
+ *           description: Project's description
+ *         status:
+ *           type: string
+ *           description: Could be "Not started", "In Progress" or "Completed"
+ *         progress:
+ *           type: number
+ *           description: Project's progress
+ *         createdAt:
+ *           type: date
+ *           description: Auto-generated date
+ *         updatedAt:
+ *           type: date
+ *           description: Auto-generated date
+ *       example:
+ *         user: 507f1f77bcf86cd799439011
+ *         title: React Web Application
+ *         description: A simple web application in react
+ *         status: Not Started
+ *         progress: 0
+ *         createdAt: 2022-02-26T17:08:13.930Z
+ *         updatedAt: 2022-02-26T17:08:13.008Z
+ */
 const projectSchema = mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
