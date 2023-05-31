bceffc819f0b87060bd02add78bce6510edf55b9
ChrisEs146 <chris.es146@gmail.com>
2023-05-30 20:22:32 -0600

    refactor: Update whitelist

--- src/utils/corsOptions.js
+++ src/utils/corsOptions.js
@@ -1,4 +1,4 @@
-const whiteList = ["https://tracku-app.netlify.app", "https://tracku-api.cyclic.app"];
+const whiteList = ["https://tracku-api.cyclic.app"];
 export const corsOptions = {
   origin: (origin, callback) => {
     if (whiteList.indexOf(origin) !== -1 || !origin) {
